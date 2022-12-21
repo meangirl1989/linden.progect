22054ace9ddde5550ee7c25f144e343e97e95411
Iva <bezrukova.ioanna@gmail.com>
2022-12-21 15:05:48 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,43 @@
-let number = 5;
-const leftBorderWidth = 1;
+// "use strict";
 
-number = 10;
-console.log(number);
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// }
+// obj.a = 10;
+
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Iva';
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+let numberOfFilms = +promt ("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+      count: numberOfFilms,
+      movies: {},
+      actors: {},
+      genres: [],
+      privat: false
+};
+
+const a = promt('Один из последних просмотренных фильмов?', ''),
+      b = promt('На сколько оцените его?', ''),
+      c = promt('Один из последних просмотренных фильмов?', ''),
+      d = promt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
